84c46ce8b80bd310b250b7224842bd8370a9096e
Chandan Kumar <chandan.kumar@imaginea.com>
2014-07-11 17:43:18 +0530

    Document config settings

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,19 @@
+'''
+Settings that drive the identification of similar nodes.
 
-MIN_DISTINCT = 3
-MAX_DISTINCT_PCT = 50
-SEARCH_TERMS = ['c3.8xlarge']
+'''
+
+# Comma-separated list of search terms
+SEARCH_TERMS = ['java', 'm3.xlarge']
+
+# Search type. Possible values: 'AND' 'OR'
 SEARCH_TYPE = 'AND'
+
+# Terms that have less distinct values than this setting are ignored.
+MIN_DISTINCT = 3
+
+# Terms that have distinct values higher than this percentage are ignored.
+MAX_DISTINCT_PCT = 50
+
+# Results that score higher than this number are considered similar.
 THRESHOLD_SIMILARITY_SCORE = 50
